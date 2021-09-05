04abb07d0eee5c1b9f370b2366d12d8519f89764
andrijaMandic <48285698+andrijaMandic@users.noreply.github.com>
2021-09-05 23:34:20 +0200

    Update LandmarkAlgorithmTest.java

--- test/spp/LandmarkAlgorithmTest.java
+++ test/spp/LandmarkAlgorithmTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package spp;
 
 import org.junit.After;
@@ -15,7 +10,7 @@ import static org.junit.Assert.*;
 /**
  *  Klasa koja sadrzi jedinicne testove za ALT algoritam.
  * 
- * @author mandi
+ * @author mandic
  */
 public class LandmarkAlgorithmTest {
     
